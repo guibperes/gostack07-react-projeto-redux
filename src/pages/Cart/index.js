b239fc8fcbdca56aff604d011a006767a08ca803
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-11-06 16:15:20 -0300

    created home and cart styles

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
 
-import { Container } from './styles'
+import { Container, ProductTable, Total } from './styles'
 
 export function Cart () {
   return (
     <Container>
-      Cart
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='produto' />
+            </td>
+            <td>
+              <strong>Tênis top</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type='button'>
+                  <MdRemoveCircleOutline color='#7159C1' size={20} />
+                </button>
+                <input type='number' readOnly value={1} />
+                <button type='button'>
+                  <MdAddCircleOutline color='#7159C1' size={20} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type='button'>
+                <MdDelete color='#7159C1' size={20} />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type='button'>Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1024,00</strong>
+        </Total>
+      </footer>
     </Container>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,89 @@
 import React from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
 
-import { Container } from './styles'
+import { ProductList } from './styles'
 
 export function Home () {
   return (
-    <Container>
-      Home
-    </Container>
+    <ProductList>
+      <li>
+        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
+        <strong>Tênis top</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart color='#FFF' size={16} />
+            3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
+        <strong>Tênis top</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart color='#FFF' size={16} />
+            3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
+        <strong>Tênis top</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart color='#FFF' size={16} />
+            3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
+        <strong>Tênis top</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart color='#FFF' size={16} />
+            3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
+        <strong>Tênis top</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart color='#FFF' size={16} />
+            3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
+        <strong>Tênis top</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart color='#FFF' size={16} />
+            3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   )
 }
