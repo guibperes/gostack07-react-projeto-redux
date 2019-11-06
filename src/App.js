177a28259f24811c392865a022b073bb8af426c7
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-11-06 12:02:32 -0300

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
+import GlobalStyles from './styles/global'
 import { Routes } from './routes'
 
 export function App () {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes />
     </BrowserRouter>
   )
