28d613f8b6d27bb1ee08db1b83bd2c9e6659f6af
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-11-20 00:02:18 -0300

    checked duplicity of add product

--- src/store/cart/reducer.js
+++ src/store/cart/reducer.js
@@ -1,13 +1,18 @@
+import { produce } from 'immer'
+
 export default function (state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1
-        }
-      ]
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id)
+
+        productIndex >= 0
+          ? draft[productIndex].amount += 1
+          : draft.push({
+            ...action.product,
+            amount: 1
+          })
+      })
     default:
       return state
   }
