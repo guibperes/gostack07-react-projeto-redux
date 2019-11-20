92a6559c91a3921182ab2eee77dd410509c85ee6
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-11-19 23:34:37 -0300

    added reactotron

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore, combineReducers } from 'redux'
 
 import cart from './cart/reducer'
 
+const enhancer = process.env.NODE_ENV === 'development'
+  ? console.tron.createEnhancer()
+  : null
+
 const reducers = combineReducers({
   cart
 })
 
-export default createStore(reducers)
+export default createStore(reducers, enhancer)
