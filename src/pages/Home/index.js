c4e3179c9bb29e51f512ec5d7b4d1db0c206d749
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-11-19 22:37:06 -0300

    listed api itens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,89 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
 
+import { api } from '../../services/api'
+import { formatPrice } from '../../utils/format'
 import { ProductList } from './styles'
 
 export function Home () {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function handleEffect () {
+      const response = await api.get('/products')
+
+      const data = response.data.map(product => ({
+        ...product,
+        price: formatPrice(product.price)
+      }))
+
+      setProducts(data)
+    }
+    handleEffect()
+  }, [])
+
   return (
     <ProductList>
-      <li>
-        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
-        <strong>Tênis top</strong>
-        <span>R$129,90</span>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.id} />
+          <strong>{product.title}</strong>
+          <span>{product.price}</span>
 
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart color='#FFF' size={16} />
+          <button type='button'>
+            <div>
+              <MdAddShoppingCart color='#FFF' size={16} />
             3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
-        <strong>Tênis top</strong>
-        <span>R$129,90</span>
-
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart color='#FFF' size={16} />
-            3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
-        <strong>Tênis top</strong>
-        <span>R$129,90</span>
-
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart color='#FFF' size={16} />
-            3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
-        <strong>Tênis top</strong>
-        <span>R$129,90</span>
-
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart color='#FFF' size={16} />
-            3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
-        <strong>Tênis top</strong>
-        <span>R$129,90</span>
-
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart color='#FFF' size={16} />
-            3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src='https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg' alt='tenis' />
-        <strong>Tênis top</strong>
-        <span>R$129,90</span>
-
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart color='#FFF' size={16} />
-            3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   )
 }
